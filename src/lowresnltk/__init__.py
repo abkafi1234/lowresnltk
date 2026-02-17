d8ffa32eafd5daf7a836c06d845e1cc90b0792e1
Ab_Kafi <58607642+abkafi1234@users.noreply.github.com>
2026-02-17 16:50:03 +0600

    Bump version to 1.1.9

--- src/lowresnltk/__init__.py
+++ src/lowresnltk/__init__.py
@@ -2,5 +2,5 @@ from .pos_tagger import POSTagger
 from .sentence_classifier import SentenceClassifier
 from .norm_evaluator import NormalizationEvaluator
 
-__version__ = "1.1.8"
+__version__ = "1.1.9"
 __all__ = ['POSTagger', 'SentenceClassifier', 'NormalizationEvaluator']
