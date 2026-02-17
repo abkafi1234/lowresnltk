45131cc2684f17d9cb81c8bd0f8716cab8aa85cc
kafi_diu_marslab <kafi.cse@diu.edu.bd>
2026-02-17 16:18:16 +0600

    testing version update

--- src/lowresnltk/__init__.py
+++ src/lowresnltk/__init__.py
@@ -2,5 +2,5 @@ from .pos_tagger import POSTagger
 from .sentence_classifier import SentenceClassifier
 from .norm_evaluator import NormalizationEvaluator
 
-__version__ = "1.1.5"
+__version__ = "1.1.6"
 __all__ = ['POSTagger', 'SentenceClassifier', 'NormalizationEvaluator']
