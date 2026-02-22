289874889945663ba09accbfe871a5dffec2465f
Ab_Kafi <58607642+abkafi1234@users.noreply.github.com>
2026-02-22 22:01:26 +0600

    Bump version to 1.1.10

--- src/lowresnltk/main.py
+++ src/lowresnltk/main.py
@@ -5,7 +5,7 @@ from .pos_tagger import POSTagger
 from .sentence_classifier import SentenceClassifier
 from .norm_evaluator import NormalizationEvaluator
 
-__version__ = "1.1.9"
+__version__ = "1.1.10"
 __all__ = ['POSTagger', 'SentenceClassifier','NormalizationEvaluator']
 
 # Model files to download
