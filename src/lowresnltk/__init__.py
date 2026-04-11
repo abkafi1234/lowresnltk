da7952b07b05ec52f721cf2de5c499fac1bfa83f
Ab_Kafi <58607642+abkafi1234@users.noreply.github.com>
2026-04-11 22:04:37 +0600

    Add UniversalClassifier and UniversalGenerator imports

--- src/lowresnltk/__init__.py
+++ src/lowresnltk/__init__.py
@@ -1,6 +1,8 @@
 from .pos_tagger import POSTagger
 from .sentence_classifier import SentenceClassifier
 from .norm_evaluator import NormalizationEvaluator
+from .classifier import UniversalClassifier
+from .generator import UniversalGenerator
 
 __version__ = "1.1.10"
 __all__ = ['POSTagger', 'SentenceClassifier','UniversalClassifier', 'UniversalGenerator', 'NormalizationEvaluator']
