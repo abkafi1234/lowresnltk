ac2bc3038bcd4365840f4b1e0697b02ca4640733
Ab_Kafi <58607642+abkafi1234@users.noreply.github.com>
2026-04-11 22:02:31 +0600

    Update __init__.py

--- src/lowresnltk/__init__.py
+++ src/lowresnltk/__init__.py
@@ -3,4 +3,4 @@ from .sentence_classifier import SentenceClassifier
 from .norm_evaluator import NormalizationEvaluator
 
 __version__ = "1.1.10"
-__all__ = ['POSTagger', 'SentenceClassifier', 'NormalizationEvaluator']
+__all__ = ['POSTagger', 'SentenceClassifier','UniversalClassifier', 'UniversalGenerator', 'NormalizationEvaluator']
